e4d67c14a06564f382d8fa03d5607091f4c45f80
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2022-08-29 16:26:11 +0700

    add project admin

---
+++ KRealEstate.AdminWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KRealEstate.AdminWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
